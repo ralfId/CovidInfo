2f99a9354fa4159515001fc57e46cdcde2406b9a
Rafael Ibañez <ralf_raid@outlook.es>
2020-11-23 22:12:40 -0600

    xaml optimization part2

--- Covid_Info.Android/obj/Debug/100/android/environment.arm64-v8a.s
+++ Covid_Info.Android/obj/Debug/100/android/environment.arm64-v8a.s
@@ -73,7 +73,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"e17650af-31b1-47b4-b9b0-931a32b244fa"
+	.asciz	"a8ad9dff-d6e3-4dba-92b8-1ab1aedbe0c3"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
